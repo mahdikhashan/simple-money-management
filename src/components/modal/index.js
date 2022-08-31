7e24f81c46b87052b111002a9a3ce6dd870ce4fc
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-08-31 19:22:06 +0430

    feat: add use-escape-key hook

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,15 +1,22 @@
+// TODO: General modal component with design patterns
+
 import React from "react";
+
 import Modal from 'react-modal'
 import Form from './form'
+
+import useEscapeKey from "../../hooks/use-escape-key";
 
 import CloseIcon from '../../assets/icons/x-regular.png'
 
 import './style.css'
 
 export const ModalView = ({modalIsOpen, setModalIsOpen}) => {
+  const isEscPressed = useEscapeKey({setModalIsOpen})
+
   return (
     <>
-      <Modal 
+      <Modal
         isOpen={modalIsOpen}
         shouldCloseOnOverlayClick={true}
         onRequestClose={setModalIsOpen}
---
+++ src/hooks/use-escape-key.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const useEscapeKey = ({ handleClose }) => {
+  const targetKey = "Escape";
+  const [isKeyPressed, setIsKeyPressed] = useState(false)
+
+  const keyUpHandler = ({ key }) => {
+    if (key === targetKey) {
+      setIsKeyPressed(false)
+    }
+  }
+
+  const keyDownHandler = ({ key }) => {
+    if (key === targetKey) {
+      setIsKeyPressed(true)
+
+      // callback
+      handleClose()
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keyup", keyUpHandler)
+    window.addEventListener("keydown", keyDownHandler)
+    return () => {
+      window.removeEventListener("keyup", keyUpHandler)
+      window.removeEventListener("keydown", keyDownHandler)
+    }
+  }, []);
+
+
+  return isKeyPressed;
+}
+
+export default useEscapeKey;
