7a7b0e021371bbbe686c3f7f29b251ab76f2cb53
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-09-01 20:38:13 +0430

    feat: react portal component

---
+++ src/components/portal/index.js
@@ -0,0 +1,28 @@
+import { useState, useLayoutEffect } from 'react'
+import { createPortal } from "react-dom";
+
+function createWrapperAndAppendToBody(wrapperId) {
+  const wrapperElement = document.createElement('div');
+  wrapperElement.setAttribute("id", wrapperId);
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+}
+
+function ReactPortal({ children, wrapperId = "react-portal-wrapper" }) {
+  const [wrapperElement, setWrapperElement] = useState(null)
+
+  useLayoutEffect(() => {
+    let element = document.getElementById((wrapperId))
+
+    if (!element) {
+      element = createWrapperAndAppendToBody(wrapperId)
+    }
+    setWrapperElement(element)
+  }, [wrapperId])
+
+  if (wrapperElement === null) return null
+
+  return createPortal(children, wrapperElement)
+}
+
+export default ReactPortal;
