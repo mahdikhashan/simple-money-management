fbe6bca674b62946c40f39887e1e61eff7515783
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-11-05 23:35:34 +0100

    update local test

--- src/index.js
+++ src/index.js
@@ -14,7 +14,23 @@ import { persistStore } from "redux-persist";
 
 import { DarkModeProvider } from "./contexts/darkMode";
 
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
 import "@Styles/index.css";
+
+i18n
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    debug: process.env.NODE_ENV === "development",
+  });
 
 const PersistGateConfig = {
   loading: null,
