cbfe23fbaef197032ef972711cef46509c29d925
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-11-07 00:33:02 +0100

    sidebar transition and toggle

--- src/components/app/SideBar/index.jsx
+++ src/components/app/SideBar/index.jsx
@@ -4,23 +4,18 @@ import Logo from "@Components/common/logo";
 
 import "./style.css";
 
-function SideBar() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
+function SideBar(props) {
+  const { isOpen, toggle } = props;
   return (
-    <aside className={``}>
+    <aside className={`${isOpen ? "open" : ""}`}>
       <div className="sidebar-header">
         <Logo />
       </div>
       <div className="sidebar-body"></div>
       <div className="sidebar-footer">
-        <button className="sidebar-roll-button" onClick={toggleSidebar}>
+        <button className="sidebar-roll-button" onClick={toggle}>
           <img
-            className="sidebar-roll-icon"
+            className={`sidebar-roll-icon ${isOpen ? "open" : ""}`}
             src={require("../../../assets/icons/arrow-right-white-50.png")}
             alt="arrow-right-sidebar"
           />
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -4,11 +4,12 @@ import { Outlet } from "react-router-dom";
 
 import SideBar from "@Components/app/SideBar";
 
-function DashboardLayout() {
+function DashboardLayout(props) {
+  const { isOpen, toggle } = props;
   return (
     <div className="container-wrapper">
-      <SideBar />
-      <main>
+      <SideBar isOpen={isOpen} toggle={toggle} />
+      <main className={`${isOpen ? "open" : ""}`}>
         <Outlet />
       </main>
     </div>
